31ef0b5aa60de8dbbce49419095a64f15ae6ab6b
borisperezg <borisperezg@gmail.com>
2022-02-25 10:19:08 -0500

    Cambios en clase

---
+++ src/u1/s1/solucion/Cliente.java
@@ -0,0 +1,13 @@
+package u1.s1.solucion;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		
+		GestionPrecio gp = new GestionPrecio();
+		
+		gp.ingresarValores();
+		
+	}
+	
+}
--- src/u1/s1/solucion/GestionPrecio.java
+++ src/u1/s1/solucion/GestionPrecio.java
@@ -1,13 +1,62 @@
 package u1.s1.solucion;
 
+import javax.swing.JOptionPane;
+
 public class GestionPrecio {
 
-	private int precios[] = new int[20];
+	private int[] precios = new int[20];
+	private double promedio;
 	
 	public void ingresarValores() {
 		
-		
+		for(int i=0;i<precios.length;i++){
+			String valor = JOptionPane.showInputDialog("Ingrese valor:");
+			int val = Integer.parseInt(valor);
+			precios[i] = val;
+		}
 		
 	}
 	
+	public void imprimirValores() {
+		for(int i=0;i<precios.length;i++){
+			System.out.println(precios[i] + " | ");
+		}
+	}
+	
+	public void imprimirSuma() {
+		int suma = calcularSuma();		
+		System.out.println("Suma = "+suma);		
+	}
+	
+	public void calcularPromedio() {
+		
+		int suma = calcularSuma();		
+		promedio = (double) suma / 20;
+		System.out.println("Prom = "+promedio);
+	}
+	
+	private int calcularSuma() {
+		int suma = 0;
+		for(int i=0;i<precios.length;i++){
+			suma += precios[i];
+		}
+		return suma;
+	}
+	
+	/**
+	 * Se requiere que antes se ejecute el método
+	 * calcularPromedio().
+	 */
+	public void imprimirMayoresPromedio() {
+		
+	}
+	
+	
 }
+
+
+
+
+
+
+
