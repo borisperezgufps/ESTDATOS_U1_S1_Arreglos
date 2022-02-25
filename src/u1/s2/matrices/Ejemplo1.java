c0b9c9a3ad5d9774e280f80142a87dfd40fe71f3
borisperezgufps <99198267+borisperezgufps@users.noreply.github.com>
2022-02-25 16:13:51 -0500

    Iniciando matrices

---
+++ src/u1/s2/matrices/Ejemplo1.java
@@ -0,0 +1,91 @@
+package u1.s2.matrices;
+
+public class Ejemplo1 {
+
+	private int[][] matriz;
+	
+	public Ejemplo1(int filas, int columnas) {
+		matriz = new int[filas][columnas];
+	}
+	
+	/**
+	 * Ingresar el valor en la fila/columna indicada por el usuario
+	 * @param fila
+	 * @param columna
+	 * @param valor
+	 */
+	public void ingresarValor(int fila, int columna, int valor) {
+		// preguntar si la matriz está instanciada
+		
+		// preguntar si la posición fila/columna existe
+		
+		// asignar el valor
+	}
+	
+	/**
+	 * Ingresa el valor en la primera fila/columna vacía
+	 * que encuentre
+	 * @param valor
+	 * @return Cadena de texto indicando la fila y columna asignadas
+	 */
+	public String ingresarValor(int valor) {
+		// preguntar si la matriz está instanciada
+		
+		// recorrer por fila y columna buscando una posición libre
+		
+		// tomar la información de la fila y columna asignadas
+		
+		return null;
+	}
+	
+	/**
+	 * Imprime todos los valores registrados en la fila indicada
+	 * Se imprimen por consola los valores separados por un guión
+	 * @param fila
+	 */
+	public void imprimirFila(int fila) {
+		
+	}
+	
+	/**
+	 * Retorna la fila (arreglo) registrado en la fila indicada
+	 * @param fila
+	 * @return
+	 */
+	public int[] retornarFila(int fila) {
+		
+		return null;
+	}
+	
+	/**
+	 * Imprime la matriz completa, una fila en cada línea.
+	 */
+	public void imprimirMatriz() {
+		
+	}
+	
+	/**
+	 * Retorna el valor registrado en la fila/columna especificada.
+	 * @param fila
+	 * @param columna
+	 * @return
+	 */
+	public int encontrarValorPorPosicion(int fila, int columna) {
+		
+		
+		return 0;
+	}
+		
+	/**
+	 * Busca en la matriz el valor indicado como parámetro
+	 * y devuelve
+	 * @param valor
+	 * @return
+	 */
+	public String encontrarPosicionDeValor(int valor) {
+		
+		
+		return null;
+	}
+	
+}
