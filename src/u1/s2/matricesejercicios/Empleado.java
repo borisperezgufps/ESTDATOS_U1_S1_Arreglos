306ce7fc36262e38b31c788df19fe1d587ac5240
Boris Perez <borisperezg@ufps.edu.co>
2023-01-26 10:17:09 -0500

    Inclusion de ejercicio

---
+++ src/u1/s2/matricesejercicios/Empleado.java
@@ -0,0 +1,29 @@
+package u1.s2.matricesejercicios;
+
+public class Empleado {
+
+	private String nombre, id;
+	private double salario;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	
+	
+}
---
+++ src/u1/s2/matricesejercicios/EmpresaPapeleria.java
@@ -0,0 +1,45 @@
+package u1.s2.matricesejercicios;
+
+public class EmpresaPapeleria {
+
+	private Empleado[][] empleados;
+	
+	public EmpresaPapeleria() {
+		
+	}
+	
+	/**
+	 * Use este metodo para enviar los datos del empleado, crear el objeto y asociarlo
+	 * al departamento correspondiente.
+	 * @param nombre
+	 * @param identificacion
+	 * @param salario
+	 * @param departamento
+	 * @return
+	 */
+	public boolean asignarEmpleadoDepartamento(String nombre, String identificacion, 
+			double salario, int departamento) {
+		
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @return Retorna el nombre del departamento al que pertenece el empleado
+	 */
+	public String buscarEmpleado(String id) {
+		
+		return null;
+	}
+	
+	/**
+	 * Retorna el nombre del empleado con el mejor sueldo
+	 * @return
+	 */
+	public String buscarEmpleadoMejorSueldo() {
+		
+		return null;
+	}
+	
+}
